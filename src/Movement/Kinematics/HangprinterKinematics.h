825191d26325c6005d099063132735ab67db8779
Torbjørn Ludvigsen <tobben@fastmail.fm>
2022-03-10 15:45:41 +0100

    Hangprinter: Adds line flex compensation

--- src/Movement/Kinematics/HangprinterKinematics.h
+++ src/Movement/Kinematics/HangprinterKinematics.h
@@ -60,23 +60,59 @@ private:
 
 	void Init() noexcept;
 	void Recalc() noexcept;
-	float LineLengthSquared(const float machinePos[3], const float anchor[3]) const noexcept;		// Calculate the square of the line length from a spool from a Cartesian coordinate
 	void ForwardTransform(float a, float b, float c, float d, float machinePos[3]) const noexcept;
 	float MotorPosToLinePos(const int32_t motorPos, size_t axis) const noexcept;
 
-	void PrintParameters(const StringRef& reply) const noexcept;									// Print all the parameters for debugging
+	void PrintParameters(const StringRef& reply) const noexcept;			// Print all the parameters for debugging
 
-	float anchors[HANGPRINTER_AXES][3];				// XYZ coordinates of the anchors
-	float printRadius;
-	// Line buildup compensation
-	float spoolBuildupFactor;
-	float spoolRadii[HANGPRINTER_AXES];
-	uint32_t mechanicalAdvantage[HANGPRINTER_AXES], linesPerSpool[HANGPRINTER_AXES];
-	uint32_t motorGearTeeth[HANGPRINTER_AXES], spoolGearTeeth[HANGPRINTER_AXES], fullStepsPerMotorRev[HANGPRINTER_AXES];
+	float printRadius = 1500.0F;
+
+	// Warning!
+	// All these default values must be calibrated by the user to fit each
+	// individual machine. M669 and M666 are normally used for setting these.
+	float anchors[HANGPRINTER_AXES][3] = {{    0.0, -2000.0, -100.0},
+	                                      { 2000.0,  1000.0, -100.0},
+	                                      {-2000.0,  1000.0, -100.0},
+	                                      {    0.0,     0.0, 3000.0}};
+
+	// Line buildup compensation configurables
+
+	/* Naive buildup factor calculation (assumes cylindrical, straight line)
+	 * line diameter: 0.5 mm
+	 * spool height: 8.0 mm
+	 * (line_cross_section_area)/(height*pi): ((0.5/2)*(0.5/2)*pi)/(8.0*pi) = 0.0078 mm
+	 * Default buildup factor for 0.50 mm FireLine: 0.0078
+	 * Default buildup factor for 0.39 mm FireLine: 0.00475
+	 * In practice you might want to compensate a bit more or a bit less */
+	float spoolBuildupFactor = 0.007;
+	float spoolRadii[HANGPRINTER_AXES] = { 75.0F, 75.0F, 75.0F, 75.0F };
+	uint32_t mechanicalAdvantage[HANGPRINTER_AXES] = { 2, 2, 2, 4 };
+	uint32_t linesPerSpool[HANGPRINTER_AXES] = { 1, 1, 1, 1 };
+	uint32_t motorGearTeeth[HANGPRINTER_AXES] = {  20,  20,  20,  20 };
+	uint32_t spoolGearTeeth[HANGPRINTER_AXES] = { 255, 255, 255, 255 };
+	uint32_t fullStepsPerMotorRev[HANGPRINTER_AXES] = { 25, 25, 25, 25};
+
+	// Flex compensation configurables
+	float moverWeight_kg = 0.0F;          // Zero disables flex compensation feature.
+	float springKPerUnitLength = 20000.0F; // Garda 1.1 is somewhere in the range [20000, 100000]
+	float minPlannedForce_Newton[HANGPRINTER_AXES] = { 0.0F, 0.0F, 0.0F, 0.0F };
+	float maxPlannedForce_Newton[HANGPRINTER_AXES] = { 70.0F, 70.0F, 70.0F, 70.0F };
+	float guyWireLengths[HANGPRINTER_AXES] = { -1.0F };
 
 	// Derived parameters
-	float k0[HANGPRINTER_AXES], spoolRadiiSq[HANGPRINTER_AXES], k2[HANGPRINTER_AXES], lineLengthsOrigin[HANGPRINTER_AXES];
-	float printRadiusSquared;
+	float k0[HANGPRINTER_AXES] = { 0.0F };
+	float spoolRadiiSq[HANGPRINTER_AXES] = { 0.0F };
+	float k2[HANGPRINTER_AXES] = { 0.0F };
+	float distancesOrigin[HANGPRINTER_AXES] = { 0.0F };
+	float springKsOrigin[HANGPRINTER_AXES] = { 0.0F };
+	float distancesWithRelaxedSpringsOrigin[HANGPRINTER_AXES] = { 0.0F };
+	float fOrigin[HANGPRINTER_AXES] = { 0.0F };
+	float printRadiusSquared = 0.0F;
+
+	float SpringK(float const springLength) const noexcept;
+	void StaticForces(float const machinePos[3], float F[4]) const noexcept;
+
+
 
 #if DUAL_CAN
 	// Some CAN helpers
