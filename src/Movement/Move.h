d277d52b98405eb87a5ef87106ea9483bebba22f
Torbjørn Ludvigsen <tobben@fastmail.fm>
2025-11-18 19:54:17 +0100

    More DDA for host build

--- src/Movement/Move.h
+++ src/Movement/Move.h
@@ -49,7 +49,13 @@ constexpr bool DirectionBackwards = !DirectionForwards;
 // A DDA represents a move in the queue.
 // Each DDA needs one DM per drive that it moves, but only when it has been prepared and frozen
 
-#if SAME70
+#if RRF_HOST_BUILD
+
+constexpr unsigned int InitialDdaRingLength = 600;
+constexpr unsigned int AuxDdaRingLength = 50;
+const unsigned int InitialNumDms = (InitialDdaRingLength/2 * 4) + AuxDdaRingLength;
+
+#elif SAME70
 
 constexpr unsigned int InitialDdaRingLength = 60;
 constexpr unsigned int AuxDdaRingLength = 5;
